b90c45b1e9255ea930cdb50e3b57699a966729e0
Zachary Fowler <zachary.fowler1@stu.ocsb.ca>
2025-10-04 00:03:00 +0000

    Practiced string literal text

---
+++ main.go
@@ -0,0 +1,28 @@
+/*
+ * @author: Zachary Fowler
+ * @version:1.0.0
+ * @date : 2025-10-3
+ * @fileoverview: This program practices with string literals.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	// In programming you often use // in front of text to tell
+	// the computer not to interpret or compile these lines.
+	// Programmers calll this comments
+
+	//string literal text
+	fmt.Println("I love Computer Science!!")
+
+	//string literal text
+	fmt.Println("    I  love Computer Science")
+
+	//string literal text
+	fmt.Println("I   love   Computer   Science   100%")
+
+	fmt.Println("\nDone")
+
+}
---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author: Zachary Fowler
+ * @version:1.0.0
+ * @date : 2025-10-3
+ * @fileoverview: This program practices with string literals.
+ */
+
+//string literal text
+console.log("I love Computer Science!!");
+
+//string literal text
+console.log("    I  love Computer Science");
+
+//string literal text
+console.log("I   love   Computer   Science   100%");
+
+console.log("\nDone");
